a084364dc1fa79728312e4cd7883504f0f7849f5
Sergey Karpushin <sergey.v.karpushin@gmail.com>
2018-10-02 19:15:29 -0400

    Added alternative user rights elevation runner

--- src/summerb/src/main/java/org/summerb/approaches/security/api/CurrentUserNotFoundException.java
+++ src/summerb/src/main/java/org/summerb/approaches/security/api/CurrentUserNotFoundException.java
@@ -5,7 +5,7 @@ import org.summerb.approaches.i18n.HasMessageCode;
 public class CurrentUserNotFoundException extends RuntimeException implements HasMessageCode {
 	private static final long serialVersionUID = 1899087866041906798L;
 
-	public static final String ERROR_LOGIN_USER_NOT_FOUND = "error.login.userNotFound";
+	public static final String ERROR_LOGIN_USER_NOT_FOUND = "error.authenticatedUserExpected";
 
 	public CurrentUserNotFoundException() {
 		super("Current user not found");
