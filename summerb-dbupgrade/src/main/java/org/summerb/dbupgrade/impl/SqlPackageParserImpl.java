af5b563d530b636851e9f33def770941e31b91fa
Sergey karpushin <Sergey.Karpushin@externals.hilti.com>
2020-05-27 10:08:53 +0300

    Impl actual Spliterator for SQL parser

--- summerb-dbupgrade/src/main/java/org/summerb/dbupgrade/impl/SqlPackageParserImpl.java
+++ summerb-dbupgrade/src/main/java/org/summerb/dbupgrade/impl/SqlPackageParserImpl.java
@@ -3,11 +3,13 @@ package org.summerb.dbupgrade.impl;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.util.StringUtils;
 import org.summerb.dbupgrade.api.SqlPackageParser;
 import org.summerb.dbupgrade.api.UpgradeStatement;
 import org.summerb.dbupgrade.utils.StringTokenizer;
@@ -26,74 +28,116 @@ public class SqlPackageParserImpl implements SqlPackageParser {
 
 	@Override
 	public Stream<UpgradeStatement> getUpgradeScriptsStream(InputStream is) throws Exception {
-		List<UpgradeStatement> ret = new ArrayList<>();
+		return StreamSupport.stream(new UpgradeStatementSpliterator(is), false);
+	}
 
-		StringTokenizer tokenizer = buildTokenizer(is);
-
+	protected class UpgradeStatementSpliterator implements Spliterator<UpgradeStatement> {
+		StringTokenizer tokenizer;
 		StringBuilder sb = new StringBuilder();
 		SubString t;
 		boolean isWithinString = false;
 		boolean isWithinSingleLineComment = false;
 		boolean isWithinMultilineComment = false;
-		while ((t = tokenizer.next()) != null) {
-			// Tokens ignoring mode
-			if (NEW_LINE == t) {
-				if (isWithinSingleLineComment) {
-					isWithinSingleLineComment = false;
-				}
-				continue;
-			}
 
-			if (isWithinSingleLineComment) {
-				continue;
-			}
-
-			if (isWithinMultilineComment) {
-				if (MULTI_LINE_COMMENT_CLOSE == t) {
-					isWithinMultilineComment = false;
-				}
-				continue;
-			}
-
-			// String capturing mode
-			if (isWithinString) {
-				if (STRING_MARKER == t) {
-					isWithinString = false;
-				}
-				sb.append(t);
-				continue;
-			}
-
-			// Entering ignoring mode
-			if (SINGLE_LINE_COMMENT == t) {
-				isWithinSingleLineComment = true;
-				continue;
-			}
-
-			if (MULTI_LINE_COMMENT_OPEN == t) {
-				isWithinMultilineComment = true;
-				continue;
-			}
-
-			if (STRING_MARKER == t) {
-				isWithinString = true;
-				sb.append(t);
-				continue;
-			}
-
-			// Statement termination
-			if (STATEMENT_END == t) {
-				sb.append(t);
-				ret.add(new UpgradeStatement(sb.toString()));
-				sb = new StringBuilder();
-				continue;
-			}
-
-			// Regular capturing
-			sb.append(t);
+		public UpgradeStatementSpliterator(InputStream is) throws Exception {
+			tokenizer = buildTokenizer(is);
 		}
-		return ret.stream();
-	}
+
+		@Override
+		public boolean tryAdvance(Consumer<? super UpgradeStatement> action) {
+			if (sb == null) {
+				return false;
+			}
+
+			while ((t = tokenizer.next()) != null) {
+				// Tokens ignoring mode
+				if (NEW_LINE == t) {
+					if (isWithinSingleLineComment) {
+						isWithinSingleLineComment = false;
+					}
+					continue;
+				}
+
+				if (isWithinSingleLineComment) {
+					continue;
+				}
+
+				if (isWithinMultilineComment) {
+					if (MULTI_LINE_COMMENT_CLOSE == t) {
+						isWithinMultilineComment = false;
+					}
+					continue;
+				}
+
+				// String capturing mode
+				if (isWithinString) {
+					if (STRING_MARKER == t) {
+						isWithinString = false;
+					}
+					sb.append(t);
+					continue;
+				}
+
+				// Entering ignoring mode
+				if (SINGLE_LINE_COMMENT == t) {
+					isWithinSingleLineComment = true;
+					continue;
+				}
+
+				if (MULTI_LINE_COMMENT_OPEN == t) {
+					isWithinMultilineComment = true;
+					continue;
+				}
+
+				if (STRING_MARKER == t) {
+					isWithinString = true;
+					sb.append(t);
+					continue;
+				}
+
+				// Statement termination
+				if (STATEMENT_END == t) {
+					sb.append(t);
+					String statement = sb.toString();
+					if (StringUtils.hasText(statement)) {
+						action.accept(new UpgradeStatement(statement));
+					}
+					sb = new StringBuilder();
+					return true;
+				}
+
+				// Regular capturing
+				sb.append(t);
+			}
+
+			// last part of script not closed by semi-colon ";"
+			if (sb.length() > 0) {
+				String statement = sb.toString();
+				if (StringUtils.hasText(statement)) {
+					action.accept(new UpgradeStatement(statement));
+				}
+				sb = null;
+				return true;
+			}
+
+			return false;
+		}
+
+		@Override
+		public long estimateSize() {
+			return Long.MAX_VALUE;
+		}
+
+		@Override
+		public int characteristics() {
+			return Spliterator.NONNULL | Spliterator.IMMUTABLE;
+		}
+
+		@Override
+		public Spliterator<UpgradeStatement> trySplit() {
+			return null;
+		}
+	};
 
 	protected StringTokenizer buildTokenizer(InputStream is) throws Exception {
 		StringTokenizer tokenizer = new StringTokenizer(read(is), STRING_MARKER, SINGLE_LINE_COMMENT,
