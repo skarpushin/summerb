c9cef8883ac36e928e5022e5b0455311c7cf4e36
Sergey Karpushin <sergey.karpushin@auriga.com>
2023-04-29 12:42:59 +0300

    Query equality check fix

--- summerb-easycrud/src/main/java/org/summerb/easycrud/api/query/restrictions/NegatableRestrictionBase.java
+++ summerb-easycrud/src/main/java/org/summerb/easycrud/api/query/restrictions/NegatableRestrictionBase.java
@@ -39,4 +39,30 @@ public abstract class NegatableRestrictionBase<T> implements CanHaveNegativeMean
 		return this;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (negative ? 1231 : 1237);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		NegatableRestrictionBase<?> other = (NegatableRestrictionBase<?>) obj;
+		if (negative != other.negative) {
+			return false;
+		}
+		return true;
+	}
+
 }
---
+++ summerb-easycrud/src/test/java/org/summerb/easycrud/api/query/QueryTest.java
@@ -0,0 +1,59 @@
+package org.summerb.easycrud.api.query;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class QueryTest {
+
+	@Test
+	public void testSameQueriesAreEqual() {
+		Query q1 = buildQuery(false);
+		Query q2 = buildQuery(false);
+		assertEquals(q1, q2);
+	}
+
+	@Test
+	public void testDifferentQueriesAreNotEqual() {
+		Query q1 = buildQuery(false);
+		Query q2 = buildQuery(true);
+		assertNotEquals(q1, q2);
+	}
+
+	protected Query buildQuery(boolean alternate) {
+		Query q = Query.n();
+		q.between("asd", 1, 2);
+		q.between("asdasd", "asd", "asd");
+		q.contains("asdad", "asd");
+		q.eq("asad", !alternate ? 2 : 1);
+		q.eq("asda", "asd");
+		q.ge("asda", 1);
+		q.in("asasd", 2L, 3L);
+		q.in("asad", "asd", "asd");
+		q.inLongs("asad", Arrays.asList(1L, 2L));
+		q.inStrings("asd", Arrays.asList("asd", "asd"));
+		q.isFalse("asd");
+		q.isNotNull("asad");
+		q.isNull("asdasd");
+		q.isTrue("asd");
+		q.le("asasd", 2L);
+		q.lengthBetween("asdasd", 1, 4);
+		q.ne("asdasd", false);
+		q.ne("asdasd", 5L);
+		q.ne("asdad", "asda");
+		q.notBetween("asdasd", 1, 5);
+		q.notContains("asdasd", "asdasd");
+		q.notIn("asasd", 2L, 3L);
+		q.notIn("asad", "asd", "asd");
+		q.notInLongs("asad", Arrays.asList(1L, 2L));
+		q.notInStrings("asd", Arrays.asList("asd", "asd"));
+		q.notLe("asda", 5L);
+		q.notStartsWith("asdasd", "asad");
+		q.or(Query.n().eq("asd", "asd"));
+		q.startsWith("asdasd", "asdasd");
+		return q;
+	}
+
+}
